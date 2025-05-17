1a244b3ad69e52c42919265b3b7b94db59f4cedb
Issatay Massalin <massalin.issatay@gmail.com>
2025-05-17 12:45:35 +0500

    update of configs to host on firebase

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/unity_webgl_integration/',
-  plugins: [react()],
+  base: '/',
+  // plugins: [react()],
 });
